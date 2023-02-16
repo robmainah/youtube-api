da4359deccc6b86fde3675a8e1916655183a18fc
robmainah <robmainah@gmail.com>
2023-02-16 10:55:59 +0300

    like a video

--- oauth-2.py
+++ oauth-2.py
@@ -1,5 +1,9 @@
 from apiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 def get_google_authorization():
     CLIENT_SECRET_FILE = 'client_secret.json'
@@ -7,9 +11,18 @@ def get_google_authorization():
 
     flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
     credentials = flow.run_local_server()
-    
-    youtube = build('youtube', 'v3', credentials=credentials)
 
-    print(youtube)
+    response = build('youtube', 'v3', credentials=credentials)
 
-get_google_authorization()
+    print(response)
+    return response
+
+
+def get_to_like_video(auth):
+    response = auth.videos().rate(rating='like', id='ArrNCcIXSIk').execute()
+    print(response)
+    print("Successful")
+
+
+auth = get_google_authorization()
+get_to_like_video(auth)
