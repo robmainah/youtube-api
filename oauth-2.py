c29b8eefd9b54bbf29c13731e422681416aff202
robmainah <robmainah@gmail.com>
2023-02-16 11:30:17 +0300

    comment on video

--- oauth-2.py
+++ oauth-2.py
@@ -7,7 +7,7 @@ environ.Env.read_env()
 
 def get_google_authorization():
     CLIENT_SECRET_FILE = 'client_secret.json'
-    SCOPES = ['https://www.googleapis.com/auth/youtube']
+    SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
 
     flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
     credentials = flow.run_local_server()
@@ -26,13 +26,33 @@ def get_to_like_video(auth):
 
 def get_video_comments():
     request = build('youtube', 'v3', developerKey=env('YOUTUBE_API_KEY'))
-    response = request.commentThreads().list(
+    results = request.commentThreads().list(
         part='snippet', videoId='jWh0FaRRZC4', maxResults=1
     ).execute()
 
-    print(response['items'])
+    print(results['items'])
 
 
-# auth = get_google_authorization()
+def comment_on_video(auth):
+    request = auth.commentThreads().insert(
+        part='snippet',
+        body={
+            "snippet": {
+                "videoId": 'jWh0FaRRZC4',
+                "topLevelComment": {
+                    "snippet": {
+                        "textOriginal": "I love the video"
+                    }
+                }
+            }
+        }
+    )
+    response = request.execute()
+
+    print(response)
+    
+
+auth = get_google_authorization()
 # get_to_like_video(auth)
-get_video_comments()
+# get_video_comments()
+comment_on_video(auth)
